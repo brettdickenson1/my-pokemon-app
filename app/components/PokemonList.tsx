50e5e6cf417878a306b4efdcc5f3fd2f20657e0d
Brett Dickenson <46332464+brettdickenson1@users.noreply.github.com>
2024-03-17 14:01:10 +0000

    finished-app

--- app/components/PokemonList.tsx
+++ app/components/PokemonList.tsx
@@ -117,7 +117,7 @@ const PokemonList: React.FC = () => {
         )}
 
         {!initialLoading && filteredPokemons.length > 0 && (
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
             <Suspense
               fallback={
                 <div className="flex items-center">
